dd5dee5e10223c0c107fd53a72d769128d562013
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2026-05-30 01:03:55 +0545

    made it so that it listens on all interfaces and added a log befoere the server starts

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,8 @@ func main() {
 	// simple http server with one endpoint
 
 	http.HandleFunc("/", rateLimitAndIpBan(dataAccessHandler))
-	err := http.ListenAndServe(":8080", nil) // nil means defualt we can pass other too like chi gorillamux router as well
+	log.Println("Server is listening on port 8080 on all interfaces")
+	err := http.ListenAndServe("0.0.0.0:8080", nil) // nil means defualt we can pass other too like chi gorillamux router as well
 	if err != nil {
 		log.Fatal("Some error while making a http server", err)
 	}
